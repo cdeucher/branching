7c94d15cae255d577040146be5e05f95217a9a97
Cristiano Deucher <cristiano.deucher@objective.com.br>
2022-03-25 17:23:28 -0300

    [DEVOPS] Add version 0.0.9

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        v0.0.8
+        v0.0.9
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
